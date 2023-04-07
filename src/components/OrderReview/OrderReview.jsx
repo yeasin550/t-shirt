1d9accd5d2964ca336f2cb94f85de4164ca540ba
yeasin550 <yeasin51570@gmail.com>
2023-04-07 10:39:13 +0600

    initial data load

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h1>this is orders page
+                
+            </h1>
+        </div>
+    );
+};
+
+export default OrderReview;
